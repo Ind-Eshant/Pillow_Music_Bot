27cff23e9a517b96c1583fb2ee64fcbba011fecd
Ind-Eshant <meshant461@gmail.com>
2025-08-17 12:26:18 +0530

    Fresh facelift commit

---
+++ config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    prefix: '!',
+    nodes: [{
+        host: "localhost",
+        password:"thor",
+        port: 6434,
+        secure: false,
+        name: "localnode"
+    }],
+    spotify: {
+        clientId: "85aab1d51a174aad9eed6d7989f530e6",
+        clientSecret: "b2ad05aa725e434c88776a1be8eab6c2"
+    },
+    botToken: "",
+    embedColor: "#0061ff"
+};
